9c24bb373a2b1fe68f38c54faa2c8877648d202b
Matthew Knowles <matthewyknowles@gmail.com>
2017-04-19 16:07:13 -0400

    Revert "we started"
    This reverts commit c7d6d687d6bd64a15a89c26600a2dbf26f25ec12.

--- spec/money.spec.ts
+++
@@ -1,9 +0,0 @@
-import { Dollar } from "../src/money";
-
-describe("Money Test", ()=> {
-    it("should test multiplication", ()=> {
-        let five: Dollar = new Dollar(5);
-        five.times(2);
-        expect(five.amount).toEqual(10);
-    });
-});
